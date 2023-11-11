7fc2b9fc21a1aeb8a3016de4a2d877f7a05ea47a
Ian Cenry <iancenry@gmail.com>
2023-11-11 18:49:51 +0300

    chore: initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia()) // state management
+app.use(router) // page/views routing
+
+app.mount('#app')
